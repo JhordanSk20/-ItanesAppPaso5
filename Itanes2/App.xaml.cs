ee2d2e149c1cc73374b86e76f6437021a3bbfed3
Retuerto Lucero Jhordan Anibal <1220648@senati.pe>
2021-12-05 20:12:03 -0500

    1

---
+++ Itanes2/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Itanes2
+{
+    public partial class App : Application
+    {
+        //public App()
+        //{
+        //InitializeComponent();
+
+        //MainPage = new MainPage();
+        //}
+        public App()
+        {
+            MainPage = new Views.BienvenidosPage
+            {
+                Content = new Label
+                {
+
+                    Text = "Bienvenidos a Itanes!",
+                    TextColor = Color.White,
+                    BackgroundColor = Color.Blue,
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
